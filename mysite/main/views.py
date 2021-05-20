8242eaf5b8d9d22094ca44887ad94d8cc5f54c77
grida <gridas01@mail.ru>
2021-05-20 12:16:03 +0700

    a

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -12,34 +12,34 @@ def index(request):
 def registration(request):
     if request.method == "POST":
         print("post")
-        username = request.POST.get('username', None)
-        # patient_patronymic = request.POST.get('patient_patronymic', None)
-        # email = request.POST.get('email', None)
-        password = request.POST.get('password', None)
-        repeat_password = request.POST.get('repeat_password', None)
-        if password != repeat_password:
-            print('Пароли не совпадают!')
-            return HttpResponseRedirect('/')
+        return render(request, "main/Reg.html")
 
-            try: user = authenticate(username=username, password=password)
-            if user is not None:
-                print('Такой пользователь уже существует')
-            else: user = User.objects.create_user(username=username, password=password)
 
-        except:  pass
-    if username is None or password is None:
-        return render(request, 'example_app/registration.html', locals())
-    if request.user.is_autnenticated:
-        return HttpResponseRedirect('/')
-    return render(request, 'example_app/registration.html', locals())
+#     username = request.POST.get('username', None)
+#     # patient_patronymic = request.POST.get('patient_patronymic', None)
+#     # email = request.POST.get('email', None)
+#     password = request.POST.get('password', None)
+#     repeat_password = request.POST.get('repeat_password', None)
+#     if password != repeat_password:
+#         print('Пароли не совпадают!')
+#         return HttpResponseRedirect('/')
+#
+#         try: user = authenticate(username=username, password=password)
+#         if user is not None:
+#             print('Такой пользователь уже существует')
+#         else: user = User.objects.create_user(username=username, password=password)
+#
+#     except:  pass
+# if username is None or password is None:
+#     return render(request, 'example_app/registration.html', locals())
+# if request.user.is_autnenticated:
+#     return HttpResponseRedirect('/')
+# return render(request, 'example_app/registration.html', locals())
 
 
 # patient_date = request.POST.get('patient_date', None)
 # policy = request.POST.get('policy', None)
 # phone = request.POST.get('phone', None)
-
-
-return render(request, "main/Reg.html")
 
 
 def personal(request):
